6552117cb6001072530e292db04613115c35f39a
arabearimanana <arabearimanana.ext@iqera.com>
2024-12-03 09:52:07 +0300

    feat: Designing simple view for creating and listing transactions

---
+++ src/main/java/mg/andy/atody/AtodyApplication.java
@@ -0,0 +1,13 @@
+package mg.andy.atody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtodyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtodyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mg/andy/atody/AtodyApplicationTests.java
@@ -0,0 +1,14 @@
+package mg.andy.atody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtodyApplicationTests {
+
+    @Test
+    void contextLoads() {
+        //context loads
+    }
+
+}
